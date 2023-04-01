443268a364daa0ceaeb41d77c060c904cf998c71
Ebubekir yigen <ebushww@gmail.com>
2023-04-01 11:04:22 +0300

    v1.4.1

--- client/src/page/Client/Basket/index.js
+++ client/src/page/Client/Basket/index.js
@@ -1,14 +1,16 @@
-import { Alert, Box, Text } from "@chakra-ui/react"
+import { Alert, Box, Button, FormControl, FormLabel, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, Textarea, useDisclosure } from "@chakra-ui/react"
 import { useSelector } from "react-redux"
 import Card from "../../../components/client/basket/Card"
 import "./index.scss"
+import { useRef } from "react"
 
 
 
 export default function Basket(){
 
+    const {isOpen, onOpen, onClose} = useDisclosure()
+    const initialRef = useRef()
     const {items} = useSelector(state => state.product)
-
     const total = items.reduce((acc, obj)=> acc + obj.price, 0)
 
     return(
@@ -19,6 +21,7 @@ export default function Basket(){
         :
         <>
             <Box mb="10"> <Text fontSize="22">Total: {total}</Text> </Box>
+            <Box mb="3"><Button onClick={onOpen} colorScheme="green" size="sm">Order</Button></Box>
             <ul className="flex">
             {
                 items.map((item)=>(
@@ -26,6 +29,31 @@ export default function Basket(){
                 ))
             }
             </ul>
+            <Modal
+            initialFocusRef={initialRef}
+            isOpen={isOpen}
+            onClose={onClose}
+            >
+                <ModalOverlay>
+                    <ModalContent>
+                        <ModalHeader>Order Now</ModalHeader>
+                        <ModalCloseButton />
+                        <ModalBody pb={6}>
+                            <FormControl>
+                                <FormLabel>Address</FormLabel>
+                                <Textarea />
+                            </FormControl>
+                        </ModalBody>
+                        <ModalFooter>
+                            <Button colorScheme="green" mr={3} >
+                                Order
+                            </Button>
+                            <Button onClick={onClose} >Cancel</Button>
+                        </ModalFooter>
+                    </ModalContent>
+                </ModalOverlay>
+
+            </Modal>
         </>
         }
         </>
--- client/src/page/Error/index.js
+++ client/src/page/Error/index.js
@@ -1,4 +1,4 @@
-import { Alert, AlertDescription, AlertIcon, AlertTitle, CloseButton } from "@chakra-ui/react";
+import { Alert, AlertDescription, AlertIcon, AlertTitle } from "@chakra-ui/react";
 
 
 export default function Error(){
