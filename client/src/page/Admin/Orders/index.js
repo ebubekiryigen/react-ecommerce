0a6145e6272c84aae3e138b77edf960e2f3d643a
Ebubekir yigen <ebushww@gmail.com>
2023-04-30 12:29:37 +0300

    v1.6.1

--- client/src/page/Admin/Orders/index.js
+++ client/src/page/Admin/Orders/index.js
@@ -1,9 +1,35 @@
-
+import {useQuery} from "react-query"
+import {orderServices} from '../../../services'
+import { Text, Table, TableCaption, Th, Tr, Thead } from "@chakra-ui/react";
 
 export default function AdminOrders() {
+
+    const {isLoading, isError, data, error} = useQuery('admin:orders', orderServices.getOrders)
+
+    if(isLoading) {
+        return <div>Loading...</div>
+    }
+
+    if(isError) {
+        return <div>Error {error.message}</div>
+    }
+
     return(
-        <div>
-            admin orders
-        </div>
+        <>
+        <Text fontSize="2xl" p={5}>
+            Orders
+        </Text>
+
+        <Table variant="simple">
+            <TableCaption>Imperial to metric conversion factors</TableCaption>
+            <Thead>
+                <Tr>
+                    <Th>Users</Th>
+                    <Th>User</Th>
+                    <Th>Items</Th>
+                </Tr>
+            </Thead>
+        </Table>
+        </>
     )
 }
--- client/src/services/order.js
+++ client/src/services/order.js
@@ -1,11 +1,24 @@
 import axios from 'axios'
 
+//user
 export const postOrder = async(input) => {
     let config = {
         headers: {
             Authorization: localStorage.getItem('access-token'),
         }
-    }
-    const {data} = await axios.post(`${process.env.REACT_APP_BASE_API}order`, input,config)
+    };
+    const {data} = await axios.post(`${process.env.REACT_APP_BASE_API}order`, input,config);
+    return data;
+}
+
+//admin
+export const getOrders = async (inut) => {
+    let config = {
+        headers: {
+            Authorization: localStorage.getItem('access-token'),
+        }
+    };
+    const {data} = await axios.get(`${process.env.REACT_APP_BASE_API}order`,config);
+
     return data
 }
