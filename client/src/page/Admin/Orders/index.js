144f449f3dba49b6f94948d9480af94c2f4a41a0
Ebubekir yigen <ebushww@gmail.com>
2023-04-30 12:37:42 +0300

    v1.6.2

--- client/src/page/Admin/Orders/index.js
+++ client/src/page/Admin/Orders/index.js
@@ -1,6 +1,6 @@
 import {useQuery} from "react-query"
 import {orderServices} from '../../../services'
-import { Text, Table, TableCaption, Th, Tr, Thead } from "@chakra-ui/react";
+import { Text, Table, TableCaption, Th, Tr, Thead, Tbody, Td } from "@chakra-ui/react";
 
 export default function AdminOrders() {
 
@@ -23,12 +23,21 @@ export default function AdminOrders() {
         <Table variant="simple">
             <TableCaption>Imperial to metric conversion factors</TableCaption>
             <Thead>
-                <Tr>
-                    <Th>Users</Th>
-                    <Th>User</Th>
-                    <Th>Items</Th>
+                <Tr backgroundColor="black" color="white">
+                    <Th color="white" fontWeight="bold">Users</Th>
+                    <Th color="white" fontWeight="bold">User Address</Th>
+                    <Th color="white" fontWeight="bold">Items</Th>
                 </Tr>
             </Thead>
+            <Tbody>
+                {data.map((item)=> (
+                    <Tr key={item._id}>
+                        <Td>{item.user.email}</Td>
+                        <Td>{item.adress}</Td>
+                        <Td>{item.items.length}</Td>
+                    </Tr>
+                ))}
+            </Tbody>
         </Table>
         </>
     )
